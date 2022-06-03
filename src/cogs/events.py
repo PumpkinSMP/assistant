37283c5017eb5c0631d55f9903696892f556d0e7
Vaibhav Dhiman <mousemoosz@protonmail.com>
2022-06-03 15:39:19 +0530

    feat: delete messages in link-account

---
+++ src/cogs/events.py
@@ -0,0 +1,40 @@
+import nextcord
+import nextcord.ext
+from nextcord.ext import commands
+import nextcord.utils as utils
+import asyncio
+
+
+class Events(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener(name="on_message")
+    async def delete_link_account(self, message):
+        """
+        Delete the messages sent in link-account channel.
+        """
+        await self.bot.wait_until_ready()
+        channel = self.bot.get_channel(961640173463023646)
+        target_bot = self.bot.get_user(960850854506815528)
+        target_message = "I don't know of such a code, try again."
+
+        if message.channel != channel: # Check if the channel is not link-account
+            return
+
+        if message.author.id in self.bot.owner_ids or message.author == self.bot.user: # Check if the message is by a team member
+            return
+
+        if message.author == target_bot:
+            asyncio.sleep(15)
+            await message.delete()
+            return
+
+        await asyncio.sleep(15)
+        await message.delete()
+        self.bot.logger.info(f"Deleted a message from {message.author} in {message.channel}")
+
+
+
+def setup(bot):
+    bot.add_cog(Events(bot))
