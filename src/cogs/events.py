8cfce813a32011089047a57331bf81db714d8f4a
Vaibhav Dhiman <mousemoosz@protonmail.com>
2022-06-20 23:46:48 +0530

    remove link account message delete
    lazypumpkin is using discordsrv dev which offers this

--- src/cogs/events.py
+++ src/cogs/events.py
@@ -10,31 +10,6 @@ class Events(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener(name="on_message")
-    async def delete_link_account(self, message):
-        """
-        Delete the messages sent in link-account channel.
-        """
-        await self.bot.wait_until_ready()
-        channel = self.bot.get_channel(961640173463023646)
-        target_bot = self.bot.get_user(960850854506815528)
-        target_message = "I don't know of such a code, try again."
-
-        if message.channel != channel: # Check if the channel is not link-account
-            return
-
-        if message.author.id in self.bot.owner_ids or message.author == self.bot.user: # Check if the message is by a team member
-            return
-
-        if message.author == target_bot:
-            await asyncio.sleep(15)
-            await message.delete()
-            return
-
-        await asyncio.sleep(15)
-        await message.delete()
-        self.bot.logger.info(f"Deleted a message from {message.author} in {message.channel}")
-
-    @commands.Cog.listener(name="on_message")
     async def pumpkin_listen(self, message):
         """
         DMs LazyPumpkin if a message contains 'pumpkin'
