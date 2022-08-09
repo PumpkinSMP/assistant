7c70530007b1f3924a71ed83520ab6cd2ea3e3b1
VaibhavSys <vaibhavsys@protonmail.com>
2022-08-09 20:46:10 +0530

    fix: treat all arguments after reason as string too

--- src/cogs/fake_mod.py
+++ src/cogs/fake_mod.py
@@ -7,11 +7,11 @@ class FakeMod(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def bon(self, ctx, member: nextcord.Member, reason: str = None):
+    async def bon(self, ctx, member: nextcord.Member, *, reason: str = None):
         await ctx.reply(f'Banned {member} for "{reason}".', mention_author=False)
 
     @commands.command()
-    async def kock(self, ctx, member: nextcord.Member, reason: str = None):
+    async def kock(self, ctx, member: nextcord.Member, *, reason: str = None):
         await ctx.reply(f'Kicked {member} for "{reason}".', mention_author=False)
 
 
